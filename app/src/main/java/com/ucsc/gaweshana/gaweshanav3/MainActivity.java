33883cae5717242a5df8ce0f19ff9e5200e4ac1f
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2015-08-02 10:34:54 +0530

    Added QR Scanner

--- app/src/main/java/com/ucsc/gaweshana/gaweshanav3/MainActivity.java
+++ app/src/main/java/com/ucsc/gaweshana/gaweshanav3/MainActivity.java
@@ -3,6 +3,7 @@ package com.ucsc.gaweshana.gaweshanav3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -18,7 +19,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scan(View view){
-        startActivity(new Intent(MainActivity.this, SingleActivity.class ));
+        String packageString = "com.ucsc.gaweshana.gaweshanav3";
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.setPackage(packageString);
+        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+        startActivityForResult(intent, 0);
+
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        if(requestCode == 0){
+            if(resultCode == RESULT_OK){
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Log.i("xZing", "contents: " + contents + " format: " + format); // Handle successful scan
+            }
+            else if(resultCode == RESULT_CANCELED){ // Handle cancel
+                Log.i("xZing", "Cancelled");
+            }
+        }
     }
 
     public void exit(View view) {
