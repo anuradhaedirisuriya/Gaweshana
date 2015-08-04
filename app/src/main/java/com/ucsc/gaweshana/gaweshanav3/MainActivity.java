4d68adc5c9898888139a34bf1c5a4c374421ab4c
anuradhaedirisuriya <anuradhaedirisuriya@gmail.com>
2015-08-05 01:39:41 +0530

    connected QR result with new activity

--- app/src/main/java/com/ucsc/gaweshana/gaweshanav3/MainActivity.java
+++ app/src/main/java/com/ucsc/gaweshana/gaweshanav3/MainActivity.java
@@ -3,48 +3,33 @@ package com.ucsc.gaweshana.gaweshanav3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void scan(View view){
-        String packageString = "com.ucsc.gaweshana.gaweshanav3";
-        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-        intent.setPackage(packageString);
-        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-        startActivityForResult(intent, 0);
-
 
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent intent){
-        if(requestCode == 0){
-            if(resultCode == RESULT_OK){
-                String contents = intent.getStringExtra("SCAN_RESULT");
-                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                Log.i("xZing", "contents: " + contents + " format: " + format); // Handle successful scan
-            }
-            else if(resultCode == RESULT_CANCELED){ // Handle cancel
-                Log.i("xZing", "Cancelled");
-            }
-        }
-    }
 
+
+    public void feedback(View view) {
+        startActivity(new Intent(MainActivity.this, SingleActivity.class));
+    }
+    public void scan(View view) {
+        startActivity(new Intent(MainActivity.this, ScanActivity.class));
+    }
     public void exit(View view) {
         finish();
         System.exit(0);
     }
+
+
 
 
 
--- app/src/main/java/com/ucsc/gaweshana/gaweshanav3/SingleActivity.java
+++ app/src/main/java/com/ucsc/gaweshana/gaweshanav3/SingleActivity.java
@@ -1,13 +1,35 @@
 package com.ucsc.gaweshana.gaweshanav3;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
-public class SingleActivity extends AppCompatActivity {
+public class SingleActivity extends AppCompatActivity  {
+
+    TextView tv;
+    TextView tv2;
+    ImageView img;
+
 
 
     @Override
@@ -15,12 +37,38 @@ public class SingleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
 
+        String res=getIntent().getStringExtra("Result");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        tv=(TextView)findViewById(R.id.title);
+        tv.setText(res);
+
+        tv2=(TextView)findViewById(R.id.desc);
+        tv2.setText(res);
+
+        final Button btnSpeak = (Button) findViewById(R.id.btnSpeak);
+
+        new NetLink().execute("http://gaweshana.pe.hu/",res);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //actionButton = (FloatingActionButton) findViewById(R.id.home_logo);
+
+        ImageView icon = new ImageView(this); // Create an icon
+        icon.setImageResource(R.drawable.home_logo);
+
+       /* FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                .setContentView(icon)
+                .build(); */
+
     }
+
+
+
+
 
 
     @Override
@@ -44,4 +92,71 @@ public class SingleActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    class NetLink extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            return GET(params[0]+params[1]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tv.setText(s);
+            try {
+                JSONObject jo=new JSONObject(s);
+                tv.setText(jo.getString("name"));
+                tv2.setText(jo.getString("description"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        public String GET(String url){
+            InputStream inputStream = null;
+            String result = "";
+            try {
+
+                // create HttpClient
+
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // make GET request to the given URL
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if(inputStream != null)
+                    result = convertInputStreamToString(inputStream);
+                else
+                    result = "Did not work!";
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+            }
+            //Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+            return result;
+        }
+        private String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+            String line = "";
+            String result = "";
+            while((line = bufferedReader.readLine()) != null)
+                result += line;
+
+
+            return result;
+
+        }
+    }
+
+
 }
